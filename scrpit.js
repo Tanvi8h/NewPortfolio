36287778cece3f38560ae1c8867bd545967666c8
Tanvi Hayer <thayer3@gatech.edu>
2024-12-02 00:46:02 -0500

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+document.querySelector('a').addEventListener('click', function () {
+    document.body.classList.add('fade-out');
+});
